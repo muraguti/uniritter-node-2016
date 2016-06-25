9f7d24d54e49ff36d8bfe85f18e15769557d870d
Laisa <laisamuraguti@gmail.com>
2016-06-25 14:55:54 +0000

    adding new tests to orders

--- features/step_definitions/orders.js
+++ features/step_definitions/orders.js
@@ -3,21 +3,42 @@ module.exports = function () {
         _ = require('lodash'),
         chai = require('chai'),
         expect = chai.expect;
-        
-    this.Given(/^an existing order with a (.*) status$/, function (status) {
-        const 
-            that = this,
-            payload = {
-            data: {
-                type: 'orders',
-                attributes: {
-                    status: status,
-                    items: [{ product_id: '598b04ea-8c20-4240-9c2b-1d36350a8d33', quantity: 1}]
+    
+    const payload = payload = {
+                data: {
+                    type: 'orders',
+                    attributes: {
+                        status: 'new',
+                        items: [{product_id: '598b04ea-8c20-4240-9c2b-1d36350a8d33', quantity: 1}]
                     }
                 }
-            }
-        
+    };
+    
+    this.Given(/^a valid order$/, function(){
+        return payload;
+    });
+    
+    this.When(/^I submit it to the API$/, function () {
+        const that = this;
+            return this.doHttpRequest('orders','post', payload)
+            .then((response) => {
+                that.validOrder = response.body;
+                return response;
+            })
+    });
+    
+    /*this.Then(/^I receive a success message$/, function (message) {
+        expect(this.response.data.attributes.message.to.equal(message))
+    });
+     
+    this.Then(/^the new order id$/, function (id) {
+        expect(this.response.data.attributes.id).to.equal(id)
+    });*/
 
+        
+    this.Given(/^an existing order with a (.*) status$/, function (status) {
+        const that = this;
+        payload.data.attributes.status = status;
         return this.doHttpRequest('orders', 'post', payload)
         .then((response) => {
             that.existingOrder = response.body;
