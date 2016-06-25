b3ee8667910b499f6fdab99be3518533fa392bcc
Laisa <laisamuraguti@gmail.com>
2016-06-25 12:44:10 +0000

    orders.js changes

--- app/models/orders.js
+++ app/models/orders.js
@@ -8,19 +8,6 @@ module.exports = function (server) {
         schema = {
             type: 'orders',
             attributes: {
-<<<<<<< HEAD
-                total: Types.number().integer().forbidden(),
-                created_on: Types.date().forbidden(),
-                updated_on: Types.date().forbidden(),
-                status: Types.string().forbidden().valid(['new', 'invoiced', 'done', 'cancelled']).min(1),
-                items: Types.array().items(
-                    Types.object().keys({
-                        product_id: Types.string().guid().required().min(1),
-                        quantity: Types.number().integer().required().min(1),
-                        price: Types.number().forbidden()
-                    })
-                ).required().min(1)
-=======
                 total: Types.number().forbidden(),
                 created_on: Types.date().forbidden(),
                 updated_on: Types.date().forbidden(),
@@ -32,7 +19,6 @@ module.exports = function (server) {
                         price: Types.number().forbidden()
                     }).required().min(1)
                 )
->>>>>>> 3cdbd0be1321720faf325c8a77677a2ef5752e34
             }
         }
 
